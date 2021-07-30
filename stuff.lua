9273ed3f39dc887d4623f995b443d13a38c5adb3
DJ <87504262+6exYes@users.noreply.github.com>
2021-07-30 12:57:51 -0400

    Update stuff.lua

--- stuff.lua
+++ stuff.lua
@@ -12,7 +12,7 @@ ImageLabel.BackgroundColor3 = Color3.new(1, 1, 1)
 ImageLabel.BackgroundTransparency = 1
 ImageLabel.Position = UDim2.new(0, 0, 0, 0)
 ImageLabel.Size = UDim2.new(1, 0, 1, 0)
-ImageLabel.Image = "http://www.roblox.com/asset/?id=143772677"
+ImageLabel.Image = "http://www.roblox.com/asset/?id=7176728858"
 Frame:TweenPosition(UDim2.new(0, 0, 0.2, 0), "Out", "Elastic", 3)
 wait(3.01)
 Frame:TweenPosition(UDim2.new(0, 0, 1.5, 0), "Out", "Elastic", 5)
